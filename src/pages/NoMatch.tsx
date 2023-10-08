27551af23cd2a0209bab08f15882311af70b2b5b
aaliboyaci <aaliboyaci@gmail.com>
2023-10-09 00:35:01 +0300

    add 404 page

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function NoMatch() {
-  return <div>NoMatch</div>;
+  return (
+    <div className="row align-items-center" style={{ height: "65vh" }}>
+      <div className="col-3"></div>
+      <div className="col-md-6 text-center">
+        <h1> 404</h1>
+        <p>Oops the page you were looking for doesn't exist here</p>
+      </div>
+      <div className="col-3"></div>
+    </div>
+  );
 }
 
 export default NoMatch;
