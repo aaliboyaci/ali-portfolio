bdd6a3c02b162be6a0ad07a077ec3f4adcb1d409
aaliboyaci <aaliboyaci@gmail.com>
2023-10-08 15:14:47 +0300

    add frontend services

---
+++ src/components/FrontendServices.tsx
@@ -0,0 +1,159 @@
+import React from "react";
+
+function FrontendServices() {
+  return (
+    <>
+      <div className="row text-center">
+        {" "}
+        <h4
+          style={{
+            paddingTop: "10px",
+            marginBottom: "20px",
+            paddingBottom: "20px",
+          }}
+        >
+          Frontend Development
+        </h4>
+      </div>
+      <div className="row">
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingSeven">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseSeven"
+                aria-expanded="false"
+                aria-controls="collapseSeven"
+              >
+                Web Application Development
+              </button>
+            </h2>
+            <div
+              id="collapseSeven"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingSeven"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Developing complex web applications with rich user interfaces
+                using frontend libraries and frameworks such as{" "}
+                <code>React, Next.js.</code>
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingEight">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseEight"
+                aria-expanded="false"
+                aria-controls="collapseEight"
+              >
+                Website Development
+              </button>
+            </h2>
+            <div
+              id="collapseEight"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingEight"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Building static or dynamic websites using HTML, CSS, and
+                JavaScript to create the visual layout, styles, and
+                interactivity.
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingNine">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseNine"
+                aria-expanded="false"
+                aria-controls="collapseNine"
+              >
+                Responsive Design
+              </button>
+            </h2>
+            <div
+              id="collapseNine"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingNine"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Ensuring that websites and applications are optimized for
+                various devices and screen sizes, providing a seamless
+                experience on desktops, tablets, and mobile phones.
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingTen">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseTen"
+                aria-expanded="false"
+                aria-controls="collapseTen"
+              >
+                Single-Page Applications (SPAs)
+              </button>
+            </h2>
+            <div
+              id="collapseTen"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingTen"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Building SPAs that provide a smooth and uninterrupted user
+                experience by loading content dynamically without full page
+                refreshes.
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingEleven">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseEleven"
+                aria-expanded="false"
+                aria-controls="collapseEleven"
+              >
+                API Integrations
+              </button>
+            </h2>
+            <div
+              id="collapseEleven"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingEleven"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Integrating data and functionality from backend services and
+                APIs into the frontend to provide dynamic and up-to-date
+                content. <code>Axios, GraphQL</code>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default FrontendServices;
---
+++ src/components/GraphicServices.tsx
@@ -0,0 +1,180 @@
+import React from "react";
+
+function GraphicServices() {
+  return (
+    <>
+      <div className="row text-center">
+        {" "}
+        <h4
+          style={{
+            paddingTop: "10px",
+            marginBottom: "20px",
+            paddingBottom: "20px",
+          }}
+        >
+          Graphic Design
+        </h4>
+      </div>
+      <div className="row">
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                Branding
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Developing a cohesive visual identity for a business, including
+                logo, color schemes, typography, and brand guidelines.
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingTwo">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                Print Design
+              </button>
+            </h2>
+            <div
+              id="collapseTwo"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingTwo"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Designing materials for print, such as business cards,
+                brochures, flyers, posters, and packaging.
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingThree">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+              >
+                Web Design
+              </button>
+            </h2>
+            <div
+              id="collapseThree"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingThree"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Crafting visually appealing and user-friendly websites,
+                including the layout, typography, and overall aesthetics.
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingFour">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseFour"
+                aria-expanded="false"
+                aria-controls="collapseFour"
+              >
+                Social Media Graphics
+              </button>
+            </h2>
+            <div
+              id="collapseFour"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingFour"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Designing visuals for social media platforms, including posts,
+                banners, and profile images.
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingFive">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseFive"
+                aria-expanded="false"
+                aria-controls="collapseFive"
+              >
+                Event Graphics
+              </button>
+            </h2>
+            <div
+              id="collapseFive"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingFive"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Creating visuals for events, including banners, posters,
+                invitations, and promotional materials.
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingSix">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseSix"
+                aria-expanded="false"
+                aria-controls="collapseSix"
+              >
+                Corporate Collateral
+              </button>
+            </h2>
+            <div
+              id="collapseSix"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingSix"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                Designing materials like business cards, letterheads, and
+                envelopes to maintain a consistent corporate image.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default GraphicServices;
